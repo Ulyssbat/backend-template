14a55bd5f610db09c9f1c3b0a1df931c8b97dbfa
Ulyssbat <stewartbatallasminda@gmail.com>
2022-06-10 20:38:58 +0200

    conseguimos conectar react con nodejs

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var testAPIRouter = require("./routes/testAPI");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
 var cors = require('cors');
 var app = express();
 var bodyParser = require('body-parser');
@@ -19,16 +20,16 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
-app.use('/', indexRouter);
+app.use('/index', indexRouter);
 app.use('/users', usersRouter);
-
+app.use("/testAPI", testAPIRouter);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
